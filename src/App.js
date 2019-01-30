3d9ea762da3cab94492f0cfc7aefce047064d82d
talentamaulana10 <tatamf.id@gmail.com>
2019-01-30 22:37:24 +0700

    add page about , contact,devloper,programing class

--- src/App.js
+++ src/App.js
@@ -2,12 +2,24 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Home from './screen/home/Home';
+import {Route, Switch} from "react-router-dom"
+import Class from './screen/class/Class'
+import About from './screen/about/About'
+import Developer from './screen/developer/Developer'
+import Contact from './screen/contact/Contact'
+
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Home/>
+        <Switch>
+                <Route path="/" exact component={Home} />
+                <Route path="/class" exact  component={Class} />
+                <Route path="/about" exact  component={About} />
+                <Route path="/contact" exact  component={Contact} />
+                <Route path="/developer" exact  component={Developer} />
+        </Switch>
       </div>
     );
   }
