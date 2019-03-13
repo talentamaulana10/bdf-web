1365ac98c91c0ac25403bc1b71d7344214b6da40
talentamaulana10 <tatamf.id@gmail.com>
2019-03-13 10:58:52 +0700

    Updates

---
+++ precache-manifest.a6e079268df80d9e8993332dd634733e.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "0fab664b4da976dbcc32",
+    "url": "/bdf-web/static/css/main.029dd734.chunk.css"
+  },
+  {
+    "revision": "0fab664b4da976dbcc32",
+    "url": "/bdf-web/static/js/main.0fab664b.chunk.js"
+  },
+  {
+    "revision": "0dfcad1394849873bd15",
+    "url": "/bdf-web/static/css/1.0d5130b7.chunk.css"
+  },
+  {
+    "revision": "0dfcad1394849873bd15",
+    "url": "/bdf-web/static/js/1.0dfcad13.chunk.js"
+  },
+  {
+    "revision": "76e5717e6c0b435954a5",
+    "url": "/bdf-web/static/js/runtime~main.76e5717e.js"
+  },
+  {
+    "revision": "e4faa3ffc9ab23df2e14aa3d068f28e2",
+    "url": "/bdf-web/static/media/tata.e4faa3ff.png"
+  },
+  {
+    "revision": "a7d20743ec1806279de086cd598f137a",
+    "url": "/bdf-web/static/media/box.a7d20743.svg"
+  },
+  {
+    "revision": "2e7ee531ef295ae6a9af6d4e4862390b",
+    "url": "/bdf-web/static/media/one.2e7ee531.png"
+  },
+  {
+    "revision": "d4c777f112e3e86375fb2eeb0ee1d5ed",
+    "url": "/bdf-web/static/media/rreact.d4c777f1.png"
+  },
+  {
+    "revision": "e029f7fc333bf90439647699277948f1",
+    "url": "/bdf-web/static/media/css.e029f7fc.png"
+  },
+  {
+    "revision": "b72f1259bb4546102d524b4132cd4033",
+    "url": "/bdf-web/static/media/js.b72f1259.png"
+  },
+  {
+    "revision": "d1eafea817348dc0a06a9c9e80360949",
+    "url": "/bdf-web/static/media/material.d1eafea8.png"
+  },
+  {
+    "revision": "f8dab57d048fabd69ea16c67e1615b86",
+    "url": "/bdf-web/static/media/node.f8dab57d.png"
+  },
+  {
+    "revision": "0aacf3522d3f3c67311dcf6b6ad9a3bb",
+    "url": "/bdf-web/static/media/bd.0aacf352.jpeg"
+  },
+  {
+    "revision": "581f46ab7346d489270cf2a5f374c4a5",
+    "url": "/bdf-web/static/media/ux.581f46ab.png"
+  },
+  {
+    "revision": "4dc7d54cf423b3ab46fe0ab56dbc3b49",
+    "url": "/bdf-web/static/media/proyek.4dc7d54c.png"
+  },
+  {
+    "revision": "32461c64f5a31beabc222b11b0582b58",
+    "url": "/bdf-web/static/media/so.32461c64.png"
+  },
+  {
+    "revision": "b75807b6b25e3a0fbfef0cff2ac480b9",
+    "url": "/bdf-web/static/media/skype.b75807b6.png"
+  },
+  {
+    "revision": "0ba2aa20e2c2ce80e9a2db5b07198464",
+    "url": "/bdf-web/static/media/github.0ba2aa20.png"
+  },
+  {
+    "revision": "acebe88437035d83d6b25f974174aa90",
+    "url": "/bdf-web/static/media/telegram.acebe884.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/bdf-web/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "e73e5087df67c71c664853f42d81feef",
+    "url": "/bdf-web/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/bdf-web/precache-manifest.a6e079268df80d9e8993332dd634733e.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/bdf-web/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
